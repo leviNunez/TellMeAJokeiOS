cebf6435986a374d8acbcec102cc1a319ed0b837
leviNunez <81485281+leviNunez@users.noreply.github.com>
2023-06-05 16:55:56 -0400

    Initial Commit

---
+++ TellMeAJoke/Model/Joke.swift
@@ -0,0 +1,19 @@
+//
+//  Joke.swift
+//  TellMeAJoke
+//
+//  Created by Levi Nunez on 5/6/23.
+//
+
+import Foundation
+
+struct Joke {
+    let type, setup, punchline: String
+    let id: Int
+    
+    static var example = Joke(
+        type: "general",
+        setup: "Why did the house go to the doctor?",
+        punchline: "It was having window panes.",
+        id: 332)
+}
---
+++ TellMeAJoke/TellMeAJokeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TellMeAJokeApp.swift
+//  TellMeAJoke
+//
+//  Created by Levi Nunez on 5/6/23.
+//
+
+import SwiftUI
+
+@main
+struct TellMeAJokeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
